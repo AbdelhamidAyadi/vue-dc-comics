dc11412b95f58e4d6cad38d0c3bf52c67d1d231f
AbdelhamidAyadi <Abdelhamid2002ayadi@gmail.com>
2022-04-20 04:41:52 +0200

    Esercizio fatto.

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,19 @@
   <div class="container">
     <header>
       <div class="logoContainer">
-        <LogoComp/>
+        <LogoComp />
       </div>
       <div class="navBarContainer">
-        <NavComp/>
+        <NavComp />
       </div>
     </header>
     <main>
-      <ContentComp/>
-      <MenuComp/>
+      <ContentComp />
+      <MenuComp />
     </main>
     <footer>
-      <FooterComp/>
-      <SocialComp/>
+      <FooterComp />
+      <SocialComp />
     </footer>
 
 
@@ -22,51 +22,52 @@
 </template>
 
 <script>
-import LogoComp from './components/LogoComp.vue'
-import NavComp from './components/NavComp.vue'
-import ContentComp from './components/ContentComp.vue'
-import MenuComp from './components/MenuComp.vue'
-import FooterComp from './components/FooterComp.vue'
-import SocialComp from './components/SocialComp.vue'
+  import LogoComp from './components/LogoComp.vue'
+  import NavComp from './components/NavComp.vue'
+  import ContentComp from './components/ContentComp.vue'
+  import MenuComp from './components/MenuComp.vue'
+  import FooterComp from './components/FooterComp.vue'
+  import SocialComp from './components/SocialComp.vue'
 
 
 
-export default {
-  name: 'App',
-  components: {
-    LogoComp,
-    NavComp,
-    ContentComp,
-    MenuComp,
-    FooterComp,
-    SocialComp
+  export default {
+    name: 'App',
+    components: {
+      LogoComp,
+      NavComp,
+      ContentComp,
+      MenuComp,
+      FooterComp,
+      SocialComp
+    },
+    
   }
-}
 </script>
 
 <style lang="scss">
-*{
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-}
-
-header{
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 10%;
-  
-  height: 70px;
-  .logoContainer,
-  .navBarContainer
-  {
-    height: 100%;
-   
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
-  
-}
-//Main part 
 
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 10%;
+
+    height: 70px;
+
+    .logoContainer,
+    .navBarContainer {
+      height: 100%;
+
+    }
+
+  }
+
+  //Main part 
 </style>
---
+++ src/components/ContentCardComp.vue
@@ -0,0 +1,38 @@
+<template>
+     <div class="card">
+       <img :src="thumb" :alt="series">
+       <p>
+         {{ series }}
+       </p>
+     </div>
+</template>
+
+<script>
+export default {
+  name: 'ContentCardComp',
+  props:{
+    thumb: String,
+    series: String
+  }
+  
+  
+}
+</script>
+
+
+<style scoped lang="scss">
+.card{
+  width: calc( 100% / 6 );
+  padding: 20px  10px;
+  img{
+    width: 100%;
+  }
+  p{
+    color: white;
+    text-transform: uppercase;
+    padding: 5px 0;
+    font-weight: bold;
+  }
+}
+
+</style>
